5938a06c89cbcd9173454debf8a7e3e8d521dec8
anthony62490 <anthonyjmagno@gmail.com>
2018-09-12 19:20:48 -0500

    Made WIP query to add image IDs to tag table

--- db/edit_photo_tags.sql
+++ db/edit_photo_tags.sql
@@ -1,21 +1,9 @@
--- THIS WORKS DON'T DELETE IT
--- Checks to see if the tag name exists in the table before adding it
--- If the name exists, the the insertion is skipped with no error
+-- Before calling this query, the user's input MUST be stripped of all duplicates and empty strings
+-- Additionally, any new tags must already be added to the tag_ref table
+-- $1 = int - picture ID
+-- $2 = str - tag
 
--- DO 
--- $do$
--- BEGIN
--- IF NOT EXISTS (SELECT * FROM test_table WHERE tag_name = $2) THEN
---   INSERT INTO test_table (tag_name) VALUES ($2);
--- END IF;
--- END
--- $do$
-
--- obtained from stackoverflow. Doesn't throw an error, but doesn't work either
--- INSERT INTO test_table (tag_name)
--- SELECT tag_name FROM (SELECT unnest($2) AS tag_name) AS arr -- unnest the array and use like a table
--- WHERE NOT EXISTS (SELECT * FROM test_table WHERE tag_name = arr.tag_name) -- ensure t is in the test
-
-INSERT INTO test_table (tag_name)
-SELECT tag_name FROM (SELECT unnest($2) AS tag_name) AS arr
-WHERE NOT EXISTS (SELECT * FROM test_table WHERE tag_name = arr.tag_name)
+INSERT INTO tag (pid,  tid)
+  SELECT $1,tag_ref.tid
+  FROM tag_ref
+  WHERE $2 = tag_ref.tag_name;
